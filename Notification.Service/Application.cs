0fb41fa762545dfa01e6ca2f4192de3c55758deb
Muhammad Qasim <ins_sheikh@hotmail.com>
2019-01-02 19:02:54 +0000

    notification service

---
+++ Notification.Service/Application.cs
@@ -0,0 +1,36 @@
+﻿using CQRS.Business.Messaging;
+using CQRS.Models;
+using MassTransit;
+using System;
+using System.Threading.Tasks;
+
+namespace Notification.Service
+{
+    public class Application
+    {
+        private readonly IBusConfig _busConfig;
+
+        public Application(IBusConfig busConfig)
+        {
+            _busConfig = busConfig;
+        }
+
+        public async Task Execute()
+        {
+            var bus = _busConfig.ConfigureBus((cfg, host) =>
+            {
+                cfg.ReceiveEndpoint(host, Queues.Notifications, n =>
+                {
+                    n.Consumer<ReceiveMessageConsumer>();
+                });
+            });
+
+            await bus.StartAsync();
+
+            Console.WriteLine("Notification service is online");
+            Console.ReadLine();
+
+            await bus.StopAsync();
+        }
+    }
+}
---
+++ Notification.Service/ReceiveMessageConsumer.cs
@@ -0,0 +1,17 @@
+﻿using CQRS.Models.Notification;
+using MassTransit;
+using System;
+using System.Threading.Tasks;
+
+namespace Notification.Service
+{
+    public class ReceiveMessageConsumer : IConsumer<INotification>
+    {
+        public Task Consume(ConsumeContext<INotification> context)
+        {
+            Console.WriteLine(context.Message.Message);
+            Console.WriteLine(string.Empty);
+            return Task.CompletedTask;
+        }
+    }
+}
